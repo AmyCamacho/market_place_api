cfb74efb64ed5db4dadc02ca11827d57d9a6f86a
Amy Camacho <amyamyc@hotmail.com>
2023-08-25 18:09:55 -0300

    Finishes modeling the product model along with user associations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
   before_action :check_owner, only: %i[update destroy]
 
+  def show
+    render json: @user
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -10,10 +14,6 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: @user.errors, status: :unprocessable_entity
     end
-  end
-
-  def show
-    render json: @user
   end
 
   def update
