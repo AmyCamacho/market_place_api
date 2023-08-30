cd9a196fa0cf67037fdbd802c533c3db9366405c
Amy Camacho <amyamyc@hotmail.com>
2023-08-29 21:08:22 -0300

    Allows the order to be placed along with product quantity

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -9,10 +9,21 @@ class OrderTest < ActiveSupport::TestCase
 
   test 'Should set total' do
     order = Order.new(user_id: @order.user_id)
-    order.products << products(:one)
-    order.products << products(:two)
+    order.products.push(products(:one))
+    order.products.push(products(:two))
     order.save
 
     assert_equal (@product1.price + @product2.price), order.total
+  end
+
+  test 'builds 2 placements for the order' do
+    @order.build_placements_with_product_ids_and_quantities [
+      { product_id: @product1.id, quantity: 2 },
+      { product_id: @product2.id, quantity: 3 },
+    ]
+
+    assert_difference('Placement.count', 2) do
+      @order.save
+    end
   end
 end
